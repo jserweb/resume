4262eae44619f37bca208d72af9a764226f1f7d2
jserweb <webjser@163.com>
2016-04-26 09:37:57 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
 $(function(){
    var angle=0;//保存旋转的角度
-   var oUl=$(".content ul").get(0);//获取旋转js对象
+   var oUl=$(".content ul").get(0),isRun=false;//获取旋转js对象
    $("#next").bind("click",next);
    $("#prev").bind("click",prev);
    function next(){
-      angle-=60;
-      turn(oUl,angle);
+      if(!isRun){
+         isRun=true;
+         angle-=60;
+         turn(oUl,angle);
+      }
+      
    }
    function prev(){
-   	 angle+=60;
-   	 turn(oUl,angle);
+      if(!isRun){
+         isRun=true;
+         angle+=60;
+         turn(oUl,angle);
+      }
+   	 
    }
-   
+   oUl.addEventListener("WebkitTransitionend",function(){
+      isRun=false;
+   },false);
+   oUl.addEventListener("transitionend",function(){
+      isRun=false;
+   },false);
 });
 function turn(obj,angle){
    	  obj.style.WebkitTransform="translateZ(-433px) rotateY("+angle+"deg)";
